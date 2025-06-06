1851690146fb25c0ba58736536f465f52af8d9c9
Leandro Melo <leandromdefreitas082@gmail.com>
2025-06-06 03:23:22 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <a target="_blank"

--- src/app/activities/[id]/page.tsx
+++ src/app/activities/[id]/page.tsx
@@ -42,7 +42,7 @@ export default function ActivityDetailPage({ params }: ActivityDetailPageProps) 
             <Button asChild className="mb-6">
               <Link href={pdfUrl} target="_blank" rel="noopener noreferrer">
                 <Download size={20} className="mr-2" />
-                VISUALIZAR ATIVIDADES
+                Visualizar Atividades
               </Link>
             </Button>
           ) : (
