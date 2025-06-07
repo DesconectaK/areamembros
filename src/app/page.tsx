b8fb532e5b563685898e2102560546c0f3d3dbb0
Leandro Melo <leandromdefreitas082@gmail.com>
2025-06-07 06:48:18 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <p class="text-bas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,7 +59,7 @@ export default function HomePage() {
             Bem-vinda(o)!
           </h1>
           <p className="text-base md:text-lg text-primary-foreground">
-            Um mundo melhor, onde vivemos sem telas. Dê ao seu filho a infância perfeita!
+            Um mundo melhor, onde vivemos sem telas. Dê ao seu filho a infância dos sonhos!
           </p>
         </div>
       </section>
